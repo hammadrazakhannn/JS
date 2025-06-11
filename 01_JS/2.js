407a4e852eae2356e5143d8a0af296f89024a9ee
Hammad RK <hammad@inventystudio.com>
2025-06-11 13:40:22 +0000

    file 2 data types

---
+++ 01_JS/2.js
@@ -0,0 +1,13 @@
+let a = null //null is an object
+let b = '33ab'
+let c = Number(b)
+let d = null;
+let e = undefined;
+let f = true;
+let g = Boolean(true);
+let h = ''
+console.log(typeof a, typeof b);
+console.log(typeof(c), c);
+console.log(d, e, f, g, h);
+
+
